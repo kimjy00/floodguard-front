7711bc11341fd99dd52042a68d78909c69c87a4f
Creeper0809 <creeper0809@hotmail.com>
2023-08-18 05:02:18 +0900

    asd

--- src/components/api/axio.js
+++ src/components/api/axio.js
@@ -2,7 +2,8 @@
 
 import axios from 'axios'; // Vuex store 가져오기
 import store from '../../store/index'
-axios.defaults.baseURL = "https://floodguard.live:8080/api/v1";
+//axios.defaults.baseURL = "https://floodguard.live:8080/api/v1"; //배포시
+axios.defaults.baseURL = "https://localhost:8080/api/v1";
 axios.interceptors.request.use(
   (config) => {
     // 로그인한 경우에만 토큰을 헤더에 추가
