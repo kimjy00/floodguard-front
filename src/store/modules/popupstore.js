93aa3716b7854952f8d4c01c580d47d250090d44
Creeper0809 <creeper0809@hotmail.com>
2023-08-18 07:01:11 +0900

    ㅁㄴㅇ

--- src/store/modules/popupstore.js
+++ src/store/modules/popupstore.js
@@ -3,7 +3,6 @@ const popupstore = {
     state : {
         loginPopUp: false,
         alertPopup: false,
-        TitlepagePopup: true,
         LogManagePopup: false,
         EvaluatePopup: false,
     },
